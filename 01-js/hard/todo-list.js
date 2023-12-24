58a81b6b0a2e5d2171356cef13b5f99b72e8da83
narayansatish <narayansatish97@gmail.com>
2023-12-24 22:03:44 +0530

    todo list 7/7 passed

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -12,6 +12,56 @@
 
 class Todo {
 
+  constructor() {
+
+    this.todosList = [];
+
+  }
+
+  add(todo) {
+
+    this.todosList.push(todo);
+
+  }
+
+  remove(indexOfTodo) {
+    if(indexOfTodo >=this.todosList.length)
+      return ;
+
+    this.todosList.splice(indexOfTodo, 1);
+
+  }
+
+  update(indexOfTodo, updatedTodo) {
+
+    if(indexOfTodo >=this.todosList.length)
+    return ;
+    this.todosList.splice(indexOfTodo, 1, updatedTodo);
+
+  }
+
+  getAll() {
+
+    return this.todosList;
+
+  }
+
+  get(indexOfTodo) {
+    if(indexOfTodo >=this.todosList.length)
+      return null;
+
+    return this.todosList[indexOfTodo];
+
+  }
+
+  clear() {
+
+    this.todosList = [];
+
+  }
+
 }
+
+ 
 
 module.exports = Todo;
