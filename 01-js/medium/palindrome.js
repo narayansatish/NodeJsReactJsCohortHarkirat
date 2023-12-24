6a3b393dc3286a08513b6eb448d77ca0bf0c26a0
narayansatish <narayansatish97@gmail.com>
2023-12-24 18:16:42 +0530

    palindrome 6/7 passed

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -2,9 +2,43 @@
   Implement a function `isPalindrome` which takes a string as argument and returns true/false as its result.
   Note: the input string is case-insensitive which means 'Nan' is a palindrom as 'N' and 'n' are considered case-insensitive.
 */
+function removePunctuation(inputString) {
+  // Define a regular expression to match punctuation marks
+  const punctuationRegex = /[!"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~]/g;
+
+  // Use replace() to remove punctuation marks
+  const stringWithoutPunctuation = inputString.replace(punctuationRegex, '');
+
+  return stringWithoutPunctuation;
+}
 
 function isPalindrome(str) {
+  str=str.toUpperCase().split(' ').join('');
+  let start = 0,end = str.length - 1;
+  str= removePunctuation(str) ;
+
+
+  while (start < end) {
+    
+    if (str[start] == str[end]) {
+
+      start++;
+
+      end--;
+
+    } else {
+
+      return false;
+
+    }
+
+  }
+
   return true;
+
 }
+
+
+ 
 
 module.exports = isPalindrome;
