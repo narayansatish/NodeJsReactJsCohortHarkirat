d38d918b4c3e04601f47e75e4317ac0e3e9f378d
narayansatish <36217851+narayansatish@users.noreply.github.com>
2024-01-02 20:55:50 +0530

    admin & user middleware added

--- week-3/03-mongo/middleware/admin.js
+++ week-3/03-mongo/middleware/admin.js
@@ -1,7 +1,15 @@
 // Middleware for handling auth
-function adminMiddleware(req, res, next) {
-    // Implement admin auth logic
-    // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+import { admin } from "..index.js";
+async function adminMiddleware(req, res, next) {
+  // Implement admin auth logic
+  // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+  let { username, password } = req.headers;
+  let value = await admin.findOne({ username: username, password: password });
+  if (value) {
+    next();
+  } else {
+    res.status(403).send("This Admin Profile does not exist. SignUp first.");
+  }
 }
 
 module.exports = adminMiddleware;
--- week-3/03-mongo/middleware/user.js
+++ week-3/03-mongo/middleware/user.js
@@ -1,6 +1,15 @@
-function userMiddleware(req, res, next) {
-    // Implement user auth logic
-    // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+import { User } from "..index.js";
+
+async function userMiddleware(req, res, next) {
+  // Implement user auth logic
+  // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+  let { username, password } = req.headers;
+  let value = await User.findOne({ username: username, password: password });
+  if (value) {
+    next();
+  } else {
+    res.status(403).send("This User Profile does not exist. SignUp first.");
+  }
 }
 
 module.exports = userMiddleware;
