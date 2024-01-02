f9484e5fd506f9072837329aea256bf3a3f809cb
narayansatish <36217851+narayansatish@users.noreply.github.com>
2024-01-02 20:56:56 +0530

    routes defined for admin & user

--- week-3/03-mongo/routes/user.js
+++ week-3/03-mongo/routes/user.js
@@ -1,22 +1,51 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
+const { Course } = require("../solution/db");
 
 // User Routes
-router.post('/signup', (req, res) => {
-    // Implement user signup logic
+router.post("/signup", async (req, res) => {
+  // Implement user signup logic
+  const username = req.body.username;
+  const password = req.body.password;
+  let user = await User.create({
+    username: username,
+    password: password,
+  });
+  res.json({
+    message: "User created successfully",
+  });
 });
 
-router.get('/courses', (req, res) => {
-    // Implement listing all courses logic
+router.get("/courses", async (req, res) => {
+  // Implement listing all courses logic
+  const response = await Course.find({});
+
+  res.json({
+    courses: response,
+  });
 });
 
-router.post('/courses/:courseId', userMiddleware, (req, res) => {
-    // Implement course purchase logic
+router.post("/courses/:courseId", userMiddleware, async (req, res) => {
+  // Implement course purchase logic
+  let courseId = req.params.courseId;
+  let username = req.headers.username;
+  let response = await User.updateOne(
+    { username: username },
+    { $push: { purchasedCourses: courseId } }
+  );
 });
 
-router.get('/purchasedCourses', userMiddleware, (req, res) => {
-    // Implement fetching purchased courses logic
+router.get("/purchasedCourses", userMiddleware, async (req, res) => {
+  // Implement fetching purchased courses logic
+  let { username } = req.headers;
+  let user = User.find({ username: username });
+  const courses = await Course.find({
+    _id: {
+      $in: user.purchasedCourses,
+    },
+  });
+  res.status(202).json({ courses: courses });
 });
 
-module.exports = router
+module.exports = router;
